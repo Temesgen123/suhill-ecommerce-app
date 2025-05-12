58a9165b9dcd464d2875a2d724ab3b4eb9bd01dc
Temesgen Birechis <birechis@gmail.com>
2025-05-12 11:18:48 +0300

    edited login page

--- backend/controller/user/userSignUp.js
+++ backend/controller/user/userSignUp.js
@@ -1,5 +1,5 @@
 const userModel = require('../../models/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 async function userSignUpController(req, res) {
   try {
@@ -17,23 +17,24 @@ async function userSignUpController(req, res) {
     if (!password) {
       throw new Error('Please enter your password');
     }
-    // const salt = bcrypt.genSaltSync(10);
-    bcrypt.hash(password, 10).then((hash) => { const payload = {
+    const salt =  bcrypt.genSaltSync(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    if (!hashPassword) {
+      throw new Error('Something went wrong while trying to encrypt password.');
+    }
+    const payload = {
       ...req.body,
       role: 'GENERAL',
-      password: hash,
+      password: hashPassword,
     };
     const userData = new userModel(payload);
-    const savedUser = userData.save();
+    const savedUser = await userData.save();
     res.status(201).json({
       data: savedUser,
       success: true,
       error: false,
       message: 'User created successfully.',
     });
-    }).catch((err) => {
-       throw new Error('Something went wrong while trying to encrypt password.');      
-    });       
   } catch (err) {
     res.json({
       message: err.message || err,
